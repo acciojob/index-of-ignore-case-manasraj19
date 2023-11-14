d193240ab8cbe704e62ec1667854c52eeee748d0
Manas Raj <119037651+manasraj19@users.noreply.github.com>
2023-11-14 22:46:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+function indexOfIgnoreCase(str, subStr) {
+  // Check for edge cases like empty strings or empty subStr
+  if (!str || !subStr) {
+    return -1;
+  }
+
+  // Convert both strings to lowercase for case-insensitive comparison
+  const lowerStr = str.toLowerCase();
+  const lowerSubStr = subStr.toLowerCase();
+
+  // Use indexOf to find the index of the lowercase subStr in the lowercase str
+  const index = lowerStr.indexOf(lowerSubStr);
+
+  return index;
 }
 
 // Please do not change the code below
